b8c67203c61c87dda582e5465a12ad71f7fb8fb1
talhasaeed22 <talhasaeed100@gmail.com>
2022-08-19 23:37:16 +0500

    Issues Fixed

--- components/Navbar.js
+++ components/Navbar.js
@@ -5,29 +5,29 @@ const Navbar = () => {
         <>
             <nav className="navbar navbar-expand-lg ">
                 <div className="container ">
-                    <a className="navbar-brand fw-bold fs-4" href="/home">BusinessBoutique.</a>
+                    <a className="navbar-brand fw-bold fs-4" >BusinessBoutique.</a>
                     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                         <span className="navbar-toggler-icon"></span>
                     </button>
                     <div className="collapse navbar-collapse" id="navbarSupportedContent" style={{justifyContent:'flex-end'}}>
                         <ul className="navbar-nav me-2 gap-3 py-3 mb-2 mb-lg-0" >
                             <li className="nav-item">
-                                <a className="nav-link text-black" aria-current="page" href="/home">Home</a>
+                                <a className="nav-link text-black" aria-current="page" >Home</a>
                             </li>
                             <li className="nav-item">
-                                <a className="nav-link text-black" href="/home">Features</a>
+                                <a className="nav-link text-black" >Features</a>
                             </li>
                             <li className="nav-item">
-                                <a className="nav-link text-black" href="/home">Reviews</a>
+                                <a className="nav-link text-black">Reviews</a>
                             </li>
                             <li className="nav-item">
-                                <a className="nav-link text-black" href="/home">Pages</a>
+                                <a className="nav-link text-black" >Pages</a>
                             </li>
                             <li className="nav-item">
-                                <a className="nav-link text-black" href="/home">Blog</a>
+                                <a className="nav-link text-black" >Blog</a>
                             </li>
                             <li className="nav-item">
-                                <a className="nav-link text-black" href="/home">FAQ</a>
+                                <a className="nav-link text-black" >FAQ</a>
                             </li>
                         </ul>
                         <form className="d-flex" role="search">
